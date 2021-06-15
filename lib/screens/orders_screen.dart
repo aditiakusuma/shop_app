7d410f1b2589081dff322380b0ea90a020ed1afd
aditia kusuma <aditiakusuma800@gmail.com>
2021-06-15 12:20:44 +0700

    add drawer

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/widgets/app_drawer.dart';
 import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
-  const OrdersScreen();
+  static const routeName = '/orders';
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +14,7 @@ class OrdersScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Orders'),
       ),
+      drawer: AppDrawer(),
       body: ListView.builder(
         itemCount: ordersData.orders.length,
         itemBuilder: (ctx, i) => OrderItem(ordersData.orders[i]),
