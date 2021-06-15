5bba5899957570806aec15b4c5185870d47bfdd7
aditia kusuma <aditiakusuma800@gmail.com>
2021-06-15 15:06:07 +0700

    updATE

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -56,8 +56,8 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
-              Scaffold.of(context).hideCurrentSnackBar();
-              Scaffold.of(context).showSnackBar(
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(
                     'Added Item to cart',
