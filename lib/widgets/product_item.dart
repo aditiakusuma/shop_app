b0c5925e82171a685812a97fb09255875bec5b4d
aditia kusuma <aditiakusuma800@gmail.com>
2021-06-15 15:02:52 +0700

    add undo add cart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -56,6 +56,21 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              Scaffold.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added Item to cart',
+                    textAlign: TextAlign.center,
+                  ),
+                  duration: Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             icon: Icon(Icons.shopping_cart),
           ),
