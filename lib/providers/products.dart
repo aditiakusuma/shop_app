52bfeb730ee407e21d100f67e3239769d6e45297
aditia kusuma <aditiakusuma800@gmail.com>
2021-06-16 12:57:27 +0700

    optimisting deleting

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:shop_app/models/http_exception.dart';
 
 import 'product.dart';
 import 'package:http/http.dart' as http;
@@ -150,8 +151,23 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url = Uri.parse(
+        'https://fire-3fefc-default-rtdb.asia-southeast1.firebasedatabase.app/products/$id.json');
+    final existingProductIndex =
+        _items.indexWhere((element) => element.id == id);
+    var existingProduct = _items[existingProductIndex];
+
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      throw HttpException('Couldnot delete product');
+    }
+    existingProduct = null;
   }
 }
