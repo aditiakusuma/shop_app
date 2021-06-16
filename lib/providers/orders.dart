54d168e5b5b4366c320e68d5893ed37c276d304b
aditia kusuma <aditiakusuma800@gmail.com>
2021-06-16 16:32:11 +0700

    fetch orders

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -25,6 +25,34 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    final url = Uri.parse(
+        'https://fire-3fefc-default-rtdb.asia-southeast1.firebasedatabase.app/orders.json');
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+            id: orderId,
+            amount: orderData['amount'],
+            dateTime: DateTime.parse(orderData['dateTime']),
+            products: (orderData['products'] as List<dynamic>).map((product) {
+              CartItem(
+                  id: product['id'],
+                  price: product['price'],
+                  title: product['title'],
+                  quantity: product['quantity']);
+            }).toList()),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     final timestamp = DateTime.now();
     final url = Uri.parse(
